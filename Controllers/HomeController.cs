c6751f6afcfacee6e17a91f3db2b7a609487f602
Sydney Hull <sydneyjhull@gmail.com>
2023-02-22 17:09:15 -0700

    yee

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,11 +37,5 @@ namespace Mission8.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ Models/TaskResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8.Models
+{
+    public class TaskResponse
+    {
+        //primary key created and hidden
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Specific Task Required")]
+        public string Task { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required(ErrorMessage ="Must select a Category")]
+        public bool Completed { get; set; }
+
+        [Required]
+        //foreign key relationships
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int QuadrantId { get; set; }
+        public Quadrant Quadrant { get; set; }
+    }
+}
