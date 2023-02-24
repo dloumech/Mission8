842f5afef8380d0bc9a89e75d9e8e9a6a8dfe502
Darby Mecham <dloumech@byu.edu>
2023-02-23 18:48:03 -0700

    context update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,12 +38,65 @@ namespace Mission8.Controllers
             return View();
         }
 
-        //need something like the following below for category data
-        //[HttpGet]
-        //public IActionResult FillOutForm()
-        //{
-        //    ViewBag.Categories = blahContext.Category.ToList();
-        //    return View("MovieForm");
+        /*
+        [HttpGet]
+        public IActionResult ViewTask()
+        {
+            var tasks = TaskSubmissionContext.Responses
+                .ToList();
+            return View(tasks);
+        }
+        [HttpGet]
+        public IActionResult Edit(int TaskId)
+        {
+            ViewBag.Categories = TaskSubmissionContext.Category.ToList();
+            var tasks = TaskSubmissionContext.Responses.Single(x => x.MovieId == applicationid);
+            return View("AddTask", application);
+        }
+        [HttpPost]
+        public IActionResult Edit(MovieResponse blah)
+        {
+            TaskSubmissionContext.Update(blah);
+            TaskSubmissionContext.SaveChanges();
+            return RedirectToAction("ViewTask");
+        }
+        [HttpGet]
+        public IActionResult Delete(int TaskId)
+        {
+            var aTaskId = TaskSubmissionContext.Responses.Single(x => x.MovieId == TaskId);
+            return View("Delete", application);
+        }
+        [HttpPost]
+        public IActionResult Delete(MovieResponse blah)
+        {
+            TaskSubmissionContext.Responses.Remove(blah);
+            TaskSubmissionContext.SaveChanges();
+            return RedirectToAction("ViewTask");
+        }
+
+        [HttpGet]
+        public IActionResult FillOutForm()
+        {
+            ViewBag.Categories = TaskSubmissionContext.Category.ToList();
+            return View("MovieForm");
+        }
+        [HttpPost]
+        public IActionResult FillOutForm(MovieResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                TaskSubmissionContext.Add(response);
+                TaskSubmissionContext.SaveChanges();
+                return View("Confirmation", response);
+            }
+            else
+            {
+                ViewBag.Categories = TaskSubmissionContext.Category.ToList();
+                return View(response);
+            }
+        }
+         */
+        // idk what where MovieId, FillOutForm(), and MovieResponse need to come from but the rest shoooould be done
         
     }
 }
