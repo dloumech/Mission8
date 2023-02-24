290b74e34072277a8f8eefd80fea55e8a5e5f82b
Zachary Irvine <irvinezachary@gmail.com>
2023-02-24 10:21:01 -0700

    Controller/Startup changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Mission8.Models;
 using System;
@@ -12,10 +13,12 @@ namespace Mission8.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private TaskSubmissionContext _taskContext { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TaskSubmissionContext taskContext)
         {
             _logger = logger;
+            _taskContext = taskContext;
         }
 
         public IActionResult Index()
@@ -23,81 +26,77 @@ namespace Mission8.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        // adding ViewTask View
-        public IActionResult ViewTask()
-        {
-            return View();
-        }
-
-        public IActionResult AddTask()
-        {
-            return View();
-        }
-
-        /*
-        [HttpGet]
-        public IActionResult ViewTask()
-        {
-            var tasks = TaskSubmissionContext.Responses
-                .ToList();
-            return View(tasks);
-        }
-        [HttpGet]
-        public IActionResult Edit(int TaskId)
-        {
-            ViewBag.Categories = TaskSubmissionContext.Category.ToList();
-            var tasks = TaskSubmissionContext.Responses.Single(x => x.MovieId == applicationid);
-            return View("AddTask", application);
-        }
-        [HttpPost]
-        public IActionResult Edit(MovieResponse blah)
-        {
-            TaskSubmissionContext.Update(blah);
-            TaskSubmissionContext.SaveChanges();
-            return RedirectToAction("ViewTask");
-        }
-        [HttpGet]
-        public IActionResult Delete(int TaskId)
-        {
-            var aTaskId = TaskSubmissionContext.Responses.Single(x => x.MovieId == TaskId);
-            return View("Delete", application);
-        }
-        [HttpPost]
-        public IActionResult Delete(MovieResponse blah)
-        {
-            TaskSubmissionContext.Responses.Remove(blah);
-            TaskSubmissionContext.SaveChanges();
-            return RedirectToAction("ViewTask");
-        }
-
         [HttpGet]
         public IActionResult FillOutForm()
         {
-            ViewBag.Categories = TaskSubmissionContext.Category.ToList();
-            return View("MovieForm");
+            ViewBag.Categories = _taskContext.Categories.ToList();
+            return View("AddTask");
         }
         [HttpPost]
-        public IActionResult FillOutForm(MovieResponse response)
+        public IActionResult FillOutForm(TaskResponse response)
         {
             if (ModelState.IsValid)
             {
-                TaskSubmissionContext.Add(response);
-                TaskSubmissionContext.SaveChanges();
-                return View("Confirmation", response);
+                _taskContext.Add(response);
+                _taskContext.SaveChanges();
+                return View("Confirmation");
             }
             else
             {
-                ViewBag.Categories = TaskSubmissionContext.Category.ToList();
-                return View(response);
+                ViewBag.Categories = _taskContext.Categories.ToList();
+                return View("AddTask", response);
             }
         }
-         */
-        // idk what where MovieId, FillOutForm(), and MovieResponse need to come from but the rest shoooould be done
-        
+
+        // adding ViewTask View
+        [HttpGet]
+        public IActionResult ViewTask()
+        {
+            var tasks = _taskContext.Responses
+                .Include(x => x.Category)
+                .OrderBy(x => x.Quadrant)
+                .ToList();
+
+            return View(tasks);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int TaskId)
+        {
+            ViewBag.Categories = _taskContext.Categories.ToList();
+            var tasks = _taskContext.Responses.Single(x => x.TaskId == TaskId);
+            return View("AddTask", tasks);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskResponse tr)
+        {
+            _taskContext.Update(tr);
+            _taskContext.SaveChanges();
+
+            return RedirectToAction("ViewTask");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int TaskId)
+        {
+            var aTaskId = _taskContext.Responses.Single(x => x.TaskId == TaskId);
+            
+            return View(aTaskId);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskResponse tr)
+        {
+            _taskContext.Responses.Remove(tr);
+            _taskContext.SaveChanges();
+            return RedirectToAction("ViewTask");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
 
